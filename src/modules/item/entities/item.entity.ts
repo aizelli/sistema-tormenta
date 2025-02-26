f973728accbe0e79fce6ec1f07b4b58790499efd
aizelli <alexandre.izelli@gmail.com>
2025-02-26 17:12:20 -0300

    atualização das entidades

--- src/modules/item/entities/item.entity.ts
+++ src/modules/item/entities/item.entity.ts
@@ -1,5 +1,18 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+export enum ItemRole {
+    ADVENTUREGEAR = "equipamentos de aventuras",
+    TOOLS = "ferramentas",
+    CLOTHING = "vestuario",
+    ESOTERIC = "esotéricos",
+    ALCHEMICAL = "alquimicos",
+    FOOD = "alimentação",
+    ANIMALS = "animais",
+    VEHICLES = "veiculos",
+    SERVICES = "serviços",
+    HIGHQUALITY = "item superior"
+}
+
 @Entity()
 export class Item {
     @PrimaryGeneratedColumn('increment')
@@ -7,4 +20,20 @@ export class Item {
 
     @Column('varchar')
     name: string;
+
+    @Column('text')
+    description: string;
+
+    @Column('decimal')
+    price: number;
+
+    @Column('integer')
+    slots: number;
+
+    @Column({
+        type: 'enum',
+        enum: ItemRole,
+        default: ItemRole.ADVENTUREGEAR,
+    })
+    type: ItemRole;
 }
--- src/modules/magic/entities/magic.entity.ts
+++ src/modules/magic/entities/magic.entity.ts
@@ -1,4 +1,22 @@
+import { createPoolCluster } from "mysql2";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+export enum ClassRole {
+    ARCANE = "arcana",
+    DIVINE = "divina",
+    CIRCLES = "circulos"
+}
+
+export enum SchoolRole {
+    ABJURATION = "abjuração",
+    DIVINATION = "adivinhação",
+    CONJURATION = "convocação",
+    ENCHANTMENT = "encantamento",
+    EVOCATION = "evocação",
+    ILLUSION = "ilusão",
+    NECROMANCY = "necromancia",
+    TRANSMUTATION = "transmutação"
+}
 
 @Entity()
 export class Magic {
@@ -7,4 +25,33 @@ export class Magic {
 
     @Column('varchar')
     name: string;
+
+    @Column('text')
+    description: string;
+
+    @Column('integer')
+    levelCircle: number;
+
+    @Column('varchar')
+    range: string;
+
+    @Column('varchar')
+    duration: string;
+
+    @Column('varchar')
+    target: string;
+
+    @Column({
+        type: 'enum',
+        enum: ClassRole,
+        default: ClassRole.ARCANE,
+    })
+    classification: ClassRole;
+
+    @Column({
+        type: 'enum',
+        enum: SchoolRole,
+        default: SchoolRole.ABJURATION,
+    })
+    school: SchoolRole;
 }
